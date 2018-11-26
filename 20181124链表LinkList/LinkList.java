74cfe64b6e21a22475171ba2cbe9d67755bc4e54
cenergy <673598118@qq.com>
2018-11-26 23:59:43 +0800

    update

--- 20181124链表LinkList/LinkList.java
+++ 20181124链表LinkList/LinkList.java
@@ -19,30 +19,141 @@ public class LinkList<E> {
         public Node() {
             this(null, null);
         }
+
         @Override
-        public String toString(){
+        public String toString() {
             return e.toString();
         }
     }
-    private  Node head;
+
+    private Node dummyHead;
     private int size;
-    public LinkList(){
-        head=null;
-        size=0;
+
+    public LinkList() {
+        dummyHead = new Node(null, null);
+        size = 0;
     }
-    public  int getSize(){
+
+    public int getSize() {
         return size;
     }
-    public boolean isEmpty(){
-        return size==0;
-    }
-    public void addFirst(E e){
-//        Node node=new Node(e);
-//        node.next=head;
-//        head=node;
-        //等价于下面一句话
-        head=new Node(e,head);
-        size++;
 
+    public boolean isEmpty() {
+        return size == 0;
     }
+
+    public void add(int index, E e) {
+        if (index < 0 || index > size) {
+            throw new IllegalArgumentException("index out of control");
+        } else {
+            Node pre = dummyHead;
+            for (int i = 0; i < index; i++) {
+                pre = pre.next;
+            }
+            pre.next = new Node(e, pre.next);
+            size++;
+        }
+    }
+
+    public void addFirst(E e) {
+        add(0, e);
+    }
+
+    public void addLast(E e) {
+        add(size, e);
+    }
+
+    public E get(int index) {
+        if (index < 0 || index > size) {
+            throw new IllegalArgumentException("index out of control");
+        }
+        Node cur = dummyHead.next;
+        for (int i = 0; i < index; i++) {
+            cur = cur.next;
+        }
+        return cur.e;
+    }
+
+    public E getFirst() {
+        return get(0);
+    }
+
+    public E getLast() {
+        return get(size - 1);
+    }
+
+    public E set(int index, E e) {
+        if (index < 0 || index > size) {
+            throw new IllegalArgumentException("index out of control");
+        }
+        Node cur = dummyHead.next;
+        for (int i = 0; i < index; i++) {
+            cur = cur.next;
+        }
+        return cur.e = e;
+    }
+
+    public boolean contains(E e) {
+        Node cur = dummyHead.next;
+        while (cur.next != null) {
+            if (cur.e.equals(e)) {
+                return true;
+            }
+            cur = cur.next;
+        }
+        return false;
+    }
+
+    public E remove(int index) {
+        if (index < 0 || index > size) {
+            throw new IllegalArgumentException("index out of control");
+        }
+        Node prev = dummyHead;
+        for (int i=0; i < index; i++) {
+            prev = prev.next;
+        }
+        Node retNode=prev.next;
+        prev.next = retNode.next;
+        retNode.next = null;
+        size--;
+        return retNode.e;
+    }
+    public E removeFirst(){
+        return remove(0);
+    }
+
+    public E removeLast(){
+        return remove(size-1);
+    }
+
+    @Override
+    public String toString() {
+        StringBuilder res = new StringBuilder();
+//        Node cur=dummyHead.next;
+//        while (cur!=null){
+//            res.append(cur+"-->");
+//            cur=cur.next;
+//        }
+        for (Node cur = dummyHead.next; cur != null; cur = cur.next) {
+            res.append(cur + "-->");
+        }
+        res.append("NULL");
+        return res.toString();
+    }
+
+    public static void main(String[] args) {
+        LinkList<Integer> linklist = new LinkList<>();
+        for (int i = 0; i < 5; i++) {
+            linklist.addLast(i);
+        }
+        linklist.add(2, 666);
+        System.out.println(linklist);
+        linklist.remove(2);
+        System.out.println(linklist);
+        linklist.removeFirst();
+        System.out.println(linklist);
+        linklist.removeLast();
+        System.out.println(linklist);
+    }
+
 }
