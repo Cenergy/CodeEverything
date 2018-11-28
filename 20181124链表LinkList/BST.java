cd796ddc89f12459e09a11b9a2b36c800fbe435a
cenergy <673598118@qq.com>
2018-11-28 22:23:47 +0800

    二分搜索树的实现

---
+++ 20181124链表LinkList/BST.java
@@ -0,0 +1,58 @@
+public class BST<E extends Comparable<E>> {
+    private class Node {
+        public E e;
+        public Node right;
+        public Node left;
+
+        public Node(E e) {
+            this.e = e;
+            right = null;
+            left = null;
+        }
+    }
+
+    private Node root;
+    private int size;
+
+    public BST() {
+        root = null;
+        size = 0;
+    }
+
+    public int getSize() {
+        return size;
+    }
+
+    public boolean isEmpty() {
+        return size == 0;
+    }
+
+    public void add(E e) {
+        if (root == null) {
+            root = new Node(e);
+            size++;
+        } else {
+            add(root, e);
+        }
+    }
+
+    //向以node为根的二分搜索树插入元素e，递归算法。
+    private void add(Node node, E e) {
+        if (e.equals(node.e)) {
+            return;
+        } else if (e.compareTo(node.e) < 0 && node.left == null) {
+            node.left = new Node(e);
+            size++;
+            return;
+        } else if (e.compareTo(node.e) > 0 && node.right == null) {
+            node.right = new Node(e);
+            size++;
+            return;
+        }
+        if (e.compareTo(node.e) < 0) {
+            add(node.left, e);
+        } else {
+            add(node.right, e);
+        }
+    }
+}
