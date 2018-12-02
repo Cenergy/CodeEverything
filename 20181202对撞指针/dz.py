5e524da2403003c25661fa5cd050abdee4d097ca
cenergy <673598118@qq.com>
2018-12-02 23:27:17 +0800

    lengthOfLongestSubstring

--- 20181202对撞指针/dz.py
+++ 20181202对撞指针/dz.py
@@ -22,5 +22,12 @@ def lengthOfLongestSubstring(s):
 
 
 if __name__ == '__main__':
+    #LeetCode3
+    # Input: "abcabcbb"
+    # Output: 3
+    # Explanation: The
+    # answer is "abc",
+    # with the length of 3.
     s = "abcdedhsjavdhsvdedadbjxzxnabcba"
     print(lengthOfLongestSubstring(s))
+    a={}
