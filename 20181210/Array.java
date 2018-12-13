202e413ea47ad1703524095506a5d2ff62ddc4b6
cenergy <673598118@qq.com>
2018-12-13 16:00:40 +0800

    二叉堆的 Array

---
+++ 20181210/Array.java
@@ -0,0 +1,148 @@
+public class Array<E> {
+    private E[] data;
+    private int size;
+
+    public Array(int capacity) {
+        data = (E[]) (new Object[capacity]);
+        size = 0;
+    }
+
+    public Array() {
+        this(10);
+    }
+
+    public int getSize() {
+        return size;
+    }
+
+    public int getCapacity() {
+        return data.length;
+    }
+
+    public boolean isEmpty() {
+        return size == 0;
+    }
+
+    public void addLast(E e) {
+        add(size, e);
+    }
+
+    public void addFirst(E e) {
+        add(0, e);
+    }
+
+    public E get(int index) {
+        if (index < 0 || index >= size)
+            throw new IllegalArgumentException("add Failed,size is full");
+        return data[index];
+    }
+
+    public E getLast() {
+        return get(size-1);
+    }
+    public E getFirst() {
+        return get(0);
+    }
+
+    public void set(int index, E e) {
+        if (index < 0 || index >= size)
+            throw new IllegalArgumentException("add Failed,size is full");
+        data[index] = e;
+    }
+
+    public void add(int index, E e) {
+
+
+        if (index < 0 || index > size) {
+            throw new IllegalArgumentException("add Failed,size is full");
+        }
+
+        if (size == data.length) {
+            resize(2 * data.length);
+        }
+
+        for (int i = size - 1; i >= index; i--) {
+            data[i + 1] = data[i];
+        }
+        data[index] = e;
+        size++;
+    }
+
+    public void resize(int newCap) {
+        E[] newData = (E[]) (new Object[newCap]);
+        for (int i = 0; i < size; i++) {
+            newData[i] = data[i];
+        }
+        data = newData;
+    }
+
+    public boolean contains(E e) {
+        for (int i = 0; i < size; i++) {
+            if (data[i].equals(e))
+                return true;
+        }
+        return false;
+    }
+
+    public int find(E e) {
+        for (int i = 0; i < size; i++) {
+            if (data[i].equals(e))
+                return i;
+        }
+        return -1;
+    }
+
+    public E remove(int index) {
+        if (index < 0 || index > size)
+            throw new IllegalArgumentException("add Failed,size is full");
+        E res = data[index];
+        for (int i = index + 1; i < size; i++) {
+            data[i - 1] = data[i];
+        }
+        data[size] = null;
+        size--;
+        if (size == data.length / 4 && data.length / 2 != 0)
+            resize(data.length / 2);
+        return res;
+    }
+
+    public E removeFirst() {
+        return remove(0);
+    }
+
+    public E removeLast() {
+        return remove(size - 1);
+    }
+
+    public void removeElement(E e) {
+        int index = find(e);
+        if (index != -1) {
+            remove(index);
+        }
+    }
+    public void swap(int i,int j){
+        if(i<0||i>=size||j<0||j>=size){
+            throw  new IllegalArgumentException("index is illegal");
+        }
+        E temp=data[i];
+        data[i]=data[j];
+        data[j]=temp;
+    }
+
+    @Override
+    public String toString() {
+        StringBuilder res = new StringBuilder();
+        res.append(String.format("Array:size=%d,capacity=%d\n", size, data.length));
+        res.append('[');
+        for (int i = 0; i < size; i++) {
+            res.append(data[i]);
+            if (i != size - 1) {
+                res.append(',');
+            }
+
+        }
+        res.append(']');
+        return res.toString();
+    }
+
+}
