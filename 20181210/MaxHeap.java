3bfcfd625bb4ddbbed2e60fbe2a66656f1887910
cenergy <673598118@qq.com>
2018-12-14 22:58:05 +0800

    二叉堆的update

--- 20181210/MaxHeap.java
+++ 20181210/MaxHeap.java
@@ -11,6 +11,14 @@ public class MaxHeap<E extends Comparable<E>> {
         data = new Array<>();
     }
 
+    //任意数组变成MaxHeap
+    public MaxHeap(E[] arr){
+        data=new Array<>(arr);
+        for (int i=parent(arr.length-1);i>=0;i--){
+            siftDown(i);
+        }
+    }
+
     public int size() {
         return data.getSize();
     }
@@ -76,25 +84,56 @@ public class MaxHeap<E extends Comparable<E>> {
             }
         }
     }
+    //用元素e来替换最大元素
+    public E replace(E e){
+        E ret=findMax();
+        data.set(0,e);
+        siftDown(0);
+        return ret;
+    }
+
+
+    private  static double testMaxHeap(Integer[] testData,boolean isHeapify){
+        long startTime=System.nanoTime();
+        MaxHeap<Integer> maxHeap;
+        if(isHeapify){
+            maxHeap=new MaxHeap<>(testData);
+        }else{
+            maxHeap=new MaxHeap<>();
+            for(int num:testData){
+                maxHeap.add(num);
+            }
+        }
+//        int[] arr=new int[testData.length];
+//        for(int i=0;i<testData.length;i++){
+//            arr[i]=maxHeap.extractMax();
+//        }
+//        for (int i=1;i<testData.length;i++){
+//            if(arr[i-1]<arr[i]){
+//                throw new IllegalArgumentException("error");
+//            }
+//        }
+        System.out.println("MaxHeap is complete");
+        long endTime=System.nanoTime();
+        return (endTime-startTime)/1000000000.0;
+    }
 
 
     public static void main(String[] args) {
-        int n=10000;
-        MaxHeap<Integer> maxheap=new MaxHeap<>();
+
+
+
+        int n=1000000;
+        Integer[] testData=new Integer[n];
         Random random=new Random();
+        MaxHeap<Integer> maxheap=new MaxHeap<>();
         for(int i=0;i<n;i++){
-            maxheap.add(random.nextInt(Integer.MAX_VALUE));
+            testData[i]=random.nextInt(Integer.MAX_VALUE);
         }
-        int[] arr=new int[n+10];
-        for(int i=0;i<n;i++){
-            arr[i]=maxheap.extractMax();
-        }
-        for (int i=1;i<n;i++){
-            if(arr[i-1]<arr[i]){
-                throw new IllegalArgumentException("error");
-            }
-        }
-        System.out.println("MaxHeap is complete");
+        double time1=testMaxHeap(testData,false);
+        System.out.println("maxHeap without heapify"+time1+"s");
+        double time2=testMaxHeap(testData,true);
+        System.out.println("maxHeap without heapify"+time2+"s");
 
     }
 
