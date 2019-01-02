e91c342b82bf80ddf020fd7d41720d7b40770ac1
helen <673598118@qq.com>
2019-01-02 23:28:11 +0800

    update

---
+++ 20190102BinarySearch/leetcode208.py
@@ -0,0 +1,28 @@
+# -*-coding:utf-8 -*-
+__author__ = 'Cenergy'
+__date__ = '2/1/2019 10:22 PM'
+
+
+
+# Given an array nums, write a function to move all 0's to the end of it
+# while maintaining the relative order of the non-zero elements.
+#
+# Example:
+#
+# Input: [0,1,0,3,12]
+# Output: [1,3,12,0,0]
+
+class Solution(object):
+    def moveZeroes(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: void Do not return anything, modify nums in-place instead.
+        """
+        i = 0
+        for j in range(len(nums)):
+            if nums[j]:
+                if i==j:
+                    pass
+                else:
+                    nums[i], nums[j] = nums[j], nums[i]
+                i+=1
