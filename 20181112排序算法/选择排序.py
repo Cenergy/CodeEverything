a25853bbbe4346b72c4acfc84fd7a14838644810
helen <673598118@qq.com>
2019-01-09 23:43:39 +0800

    select sort

--- 20181112排序算法/选择排序.py
+++ 20181112排序算法/选择排序.py
@@ -3,16 +3,19 @@ __author__ = 'Cenergy'
 __date__ = '9/1/2019 9:13 PM'
 
 
-def selectSort(nums):
-    for i in range(len(nums)):
+def selection_sort(arr):
+    for i in range(len(arr)):
         minIndex=i
-        for j in range(i+1,len(nums)):
-            if nums[minIndex]>nums[j]:
+        for j in range(i+1,len(arr)):
+            if arr[minIndex]>arr[j]:
                 minIndex=j
-        nums[i],nums[minIndex]=nums[minIndex],nums[i]
-    return nums
+        if i==minIndex:
+            pass
+        else:
+            arr[i],arr[minIndex]=arr[minIndex],arr[i]
+    return arr
 
 
 if __name__ == '__main__':
-    abc = [11, 2, 34, 23, 5]
-    print(selectSort(abc))
+    testlist = [17, 23, 20, 14, 12, 25, 1, 20, 81, 14, 11, 12]
+    print(selection_sort(testlist))
