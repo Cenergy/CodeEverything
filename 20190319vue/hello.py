e854e86b3dbd8ade48b38513b5dadb691d70afda
Cenergy <673598118@qq.com>
2019-03-30 23:54:57 +0800

    add

--- 20190319vue/hello.py
+++ 20190319vue/hello.py
@@ -1,9 +1,14 @@
-class Company:
-    def __init__(self,employee_list):
-        self.employee=employee_list
-    def __getitem__(self,item):
-        return self.employee[item+1]
+# class Company:
+#     def __init__(self,employee_list):
+#         self.employee=employee_list
+#     def __getitem__(self,item):
+#         return self.employee[item+1]
 
-company=Company([1,2,3,"abc","def"])
-for i in company:
-    print(i)
+# company=Company([1,2,3,"abc","def"])
+# for i in company:
+#     print(i)
+from geopy.geocoders import Nominatim
+
+geolocator = Nominatim()
+location = geolocator.geocode("天安门")
+print(location.address)
